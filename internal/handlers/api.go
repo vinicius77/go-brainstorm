ff9588b99dea1065948770bde994607ae06fea7a
Vinicius Cerqueira Bonifácio <vinicius.bonifacio1985@gmail.com>
2024-05-05 21:58:45 +0300

    feat(request-handlers): initial request handlers

---
+++ internal/handlers/api.go
@@ -0,0 +1,19 @@
+package handlers
+
+import (
+	"github.com/go-chi/chi"
+	chimiddleware "github.com/go-chi/chi/middleware"
+	"github.com/vinicius77/go-brainstorm/internal/middleware"
+)
+
+// Public Function
+func Handler(router *chi.Mux) {
+	router.Use(chimiddleware.StripSlashes)
+	router.Route("/account", func(r chi.Router) {
+
+		router.Use(middleware.Authorization)
+
+		router.Get("/coins", GetCoinsBalance)
+
+	})
+}
