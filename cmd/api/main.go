5fa4565c2804607047e70cf769d604fd141eeae2
Vinicius Cerqueira Bonifácio <vinicius.bonifacio1985@gmail.com>
2024-05-06 21:43:49 +0300

    feat(fiber-api): example of fiber api calls

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,12 +5,63 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi"
+	"github.com/gofiber/fiber/v2"
 	log "github.com/sirupsen/logrus"
 	"github.com/vinicius77/go-brainstorm/internal/handlers"
 )
 
+type Todo struct {
+	ID    int    `json:"id"`
+	Title string `json:"title"`
+	Done  bool   `json:"done"`
+	Body  string `json:"body"`
+}
+
 func main() {
 	log.SetReportCaller(true)
+	app := fiber.New()
+
+	todos := []Todo{}
+
+	app.Get("/health", func(c *fiber.Ctx) error {
+		return c.SendString("OK")
+	})
+
+	app.Post("/api/todos", func(c *fiber.Ctx) error {
+		todo := &Todo{}
+		err := c.BodyParser(todo)
+
+		if err != nil {
+			return err
+		}
+
+		todo.ID = len(todos) + 1
+		todos = append(todos, *todo)
+		return c.JSON(todos)
+	})
+
+	app.Patch("api/todos/:id/done", func(c *fiber.Ctx) error {
+		id, err := c.ParamsInt("id")
+
+		if err != nil {
+			return c.Status(404).SendString("Id not found")
+		}
+
+		for index, todo := range todos {
+			if todo.ID == id {
+				todos[index].Done = true
+				break
+			}
+		}
+
+		return c.JSON(todos)
+	})
+
+	app.Get("api/todos", func(c *fiber.Ctx) error {
+		return c.JSON(todos)
+	})
+
+	log.Fatal(app.Listen(":4000"))
 
 	var router *chi.Mux = chi.NewRouter()
 	handlers.Handler(router)
